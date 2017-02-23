75fbdad6d98bc09cd17cfd6caca346ef3767e2a4
itggot-jonas-rost <jonas.rost@itggot.se>
2017-02-23 11:40:45 +0100

    Gjort klart både mobilsidan och desktopsidan

--- site/js/script.js
+++ site/js/script.js
@@ -2,6 +2,19 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function popup(){
+    var person = prompt("YOU HAVE WON THE NEW IPHONE 7S! PLEASE WRITE YOUR E-MAIL ADRESS IN THE BOX BELOW:", "");
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+        "We will contact you on " + person + " soon!";
+    }
+}
+function pop_slut(){
+    alert("Alla produkter är tyvärr slut på vårt internetlager. Hör gärna av dig om du undrar något.");
+}
+
+
+
 // window.onclick = function(event) {
 //   if (!event.target.matches('.nav')) {
 
